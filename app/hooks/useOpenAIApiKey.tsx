c1ffc4133c1818ff902ce1615f10350f18de98c8
coji <coji@techtalk.jp>
2023-03-24 15:46:08 +0900

    refactor

--- app/hooks/useOpenAIApiKey.tsx
+++ app/hooks/useOpenAIApiKey.tsx
@@ -1,6 +1,22 @@
 import { useFetcher } from '@remix-run/react'
+import { json, type ActionArgs } from '@vercel/remix'
 import { useState } from 'react'
+import { z } from 'zod'
 import { AppInput } from '~/components/AppInput'
+import { sessionStorage } from '~/services/session.server'
+
+const schema = z.object({
+  apiKey: z.string(),
+})
+
+export const action = async ({ request }: ActionArgs) => {
+  const formData = await request.formData()
+  const { apiKey } = schema.parse(Object.fromEntries(formData.entries()))
+
+  const session = await sessionStorage.getSession(request.headers.get('Cookie'))
+  session.set('apiKey', apiKey)
+  return json({}, { headers: { 'Set-Cookie': await sessionStorage.commitSession(session) } })
+}
 
 export const useOpenAIApiKey = (init: string | undefined = undefined) => {
   const [apiKey, setApiKey] = useState<string | undefined>(init)
@@ -8,15 +24,7 @@ export const useOpenAIApiKey = (init: string | undefined = undefined) => {
 
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setApiKey(e.target.value)
-    fetcher.submit(
-      {
-        apiKey: e.target.value,
-      },
-      {
-        action: '/api/save-api-key',
-        method: 'post',
-      },
-    )
+    fetcher.submit({ apiKey: e.target.value }, { action: '/api/save-api-key', method: 'post' })
   }
 
   const apiKeyInput = (
--- app/routes/api.save-api-key.ts
+++ app/routes/api.save-api-key.ts
@@ -1,16 +1,1 @@
-import { json, type ActionArgs } from '@vercel/remix'
-import { z } from 'zod'
-import { sessionStorage } from '~/services/session.server'
-
-const schema = z.object({
-  apiKey: z.string(),
-})
-
-export const action = async ({ request }: ActionArgs) => {
-  const formData = await request.formData()
-  const { apiKey } = schema.parse(Object.fromEntries(formData.entries()))
-
-  const session = await sessionStorage.getSession(request.headers.get('Cookie'))
-  session.set('apiKey', apiKey)
-  return json({}, { headers: { 'Set-Cookie': await sessionStorage.commitSession(session) } })
-}
+export { action } from '~/hooks/useOpenAIApiKey'
